d1885a86a675d450e86dae4f75bb7777dfc6f8ad
Jeremy <75492743+MinhIT9@users.noreply.github.com>
2024-03-27 17:08:37 +0700

    Squashed commit of the following:
    commit 3f7ddc53317f20f2277f0a3defd9d358e04fd8c1
    Author: Jeremy <75492743+MinhIT9@users.noreply.github.com>
    Date:   Wed Mar 27 17:08:04 2024 +0700
    
        V2.5 update
    
    commit 45e8c626a134bdc211b43d6eb1b8d6d8fcbfeffc
    Merge: 5e93a88 f4d0a3d
    Author: Jeremy <75492743+MinhIT9@users.noreply.github.com>
    Date:   Wed Mar 27 17:06:39 2024 +0700
    
        Merge branch 'FormatEditMess' into FormatMess
    
    commit f4d0a3d65a2244dca49e6cfcae6005e99190fb2e
    Author: Jeremy <75492743+MinhIT9@users.noreply.github.com>
    Date:   Wed Mar 27 17:05:35 2024 +0700
    
        Update V2.5 logic mess
    
    commit a4536e810afbcff78573c2d738b986fc48281327
    Author: Jeremy <75492743+MinhIT9@users.noreply.github.com>
    Date:   Wed Mar 27 16:44:53 2024 +0700
    
        update v2.5

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, Application
 from handlers_command import set_channel, show_channels, admin, start
 from handlers_message import handle_message
-from bot_config import TOKEN_BOT, update_channels
+from bot_config import TOKEN_BOT, update_channels, CHANNELS
 import asyncio
 
 async def startup():
     print("Đang khởi động bot và cập nhật CHANNELS...")
+    global CHANNELS
     await update_channels()  # Gọi hàm cập nhật CHANNELS
     print("Khởi động thành công!")
 
